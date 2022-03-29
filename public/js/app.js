588bb2ee4c74263a48c0adce3083882ffeef4a00
Kishan patel <kishan.p@simformsolutions.com>
2022-03-29 14:10:54 +0530

    Weather APp

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageone = document.querySelector('#message-1')
+const messagetwo = document.querySelector('#message-2')
+weatherForm.addEventListener('submit', (e) => {
+
+    const placelocation = search.value
+    e.preventDefault()
+        //    console.log(placelocation)
+    messageone.textContent = 'Loading../'
+    messagetwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + placelocation).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageone.textContent = data.error
+            } else {
+                messageone.textContent = "Location:- " + data.location
+                messagetwo.textContent = "ForeCast:-  " + data.forecast
+
+            }
+
+        })
+    })
+})
